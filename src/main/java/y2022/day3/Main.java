8eeb8430ca938e67814f2685edf28e16a6d20160
pcambien <philippe.cambien@cegeka.com>
2022-12-05 16:13:53 +0100

    2022 day 3 part 1

---
+++ src/main/java/y2022/day3/Main.java
@@ -0,0 +1,21 @@
+package y2022.day3;
+
+import y2022.common.FileReader;
+
+import java.util.stream.IntStream;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int sum = FileReader.readFile(2022, 3).stream()
+                .map(l -> new String[]{l.substring(0, l.length() / 2), l.substring(l.length() / 2)})
+                .mapToInt(e -> getRank(e[0]).filter(r -> getRank(e[1]).anyMatch(s -> r == s)).findFirst().getAsInt())
+                .sum();
+
+        System.out.println("Solution part 1: " + sum);
+    }
+
+    private static IntStream getRank(String s) {
+        return s.chars().map(i -> i >= 'a' && i <= 'z' ? i - 'a' + 1 : i - 'A' + 1 + 26);
+    }
+}
